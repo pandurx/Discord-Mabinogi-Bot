ed387f3f29663b95303c67a1a27167cd4e3ddd34
Christine <internalfire5@live.com>
2018-01-23 00:40:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: BOT_TOKEN,
    autorun: true
 });
 bot.on('ready', function (evt) {
