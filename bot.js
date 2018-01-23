45974f58f3230944f1ddbf533c34155fd529c3ee
Christine <internalfire5@live.com>
2018-01-23 15:02:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,11 +17,9 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    bot.user.setGame("Hello World!");
 });
 
-bot.on('ready', () => {
-  bot.user.setGame('GAME HERE')
-})
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
