f146d20196cf9782b29cd35a19572804228c9633
Christine <internalfire5@live.com>
2018-01-23 15:36:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,11 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    
+    bot.user.setPresence( { game: {name: 'Yes boss!', type: 0 }});
 });
+
+
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
